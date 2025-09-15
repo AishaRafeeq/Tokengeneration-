f7daa35e3ed4e3e656e9a61293943128b571b150
Nishajkm <nishajkm87@gmail.com>
2025-09-15 20:39:23 +0530

    database changes

--- backend/urls.py
+++ backend/urls.py
@@ -14,12 +14,16 @@ from tokens.views import TokenViewSet
 router = DefaultRouter()
 router.register(r'tokens', TokenViewSet, basename='token')
 
+# Change the admin site header and title
+admin.site.site_header = "Token Generation Application"
+admin.site.site_title = "Token Generation Application"
+admin.site.index_title = "Welcome to the Token Generation Admin"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include('users.urls')), 
+    path('api/', include('users.urls')),
     # path('api/categories/', include('categories.urls')),
     path('api/tokens/', include('tokens.urls')),
     path('api/scans/', include('scans.urls')),
@@ -28,8 +32,6 @@ urlpatterns = [
     path('api/sidebar/', include('sidebar.urls')),
     path('api/', include(router.urls)),
     re_path(r'^app/admin/?$', lambda request: redirect('/admin/', permanent=True)),
-
-
 ]
 
 if settings.DEBUG:
